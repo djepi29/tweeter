fdafa039acf25e15fa0e7a2bbe44ff9a76e0c0ac
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-03-19 17:04:29 -0400

    implement web form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,59 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+
+
+
+
+
+
+///////////////////////
+// texteare practice
+// $().ready(() => {
+
+
+//   // Input event listener
+//   $('#textarea').on('input', function(event) {
+//     console.log('Input event happened!');
+//     // console.log(event);
+//   });
+ 
+//   // Input event listener
+//   $('#textarea').on('blur', function(event) {
+//     console.log('focus event happened!');
+//     // console.log(event);
+//   });
+
+//   // Input event listener
+//   $('#textarea').on('focus', function(event) {
+//     console.log('focus event happened!');
+//     // console.log(event);
+//   });
+ 
+//   // Input event listener
+//   $('#textarea').hover(
+//     (event) => {console.log('handlerIn')},
+//     (event) => {console.log('handlerOut')}
+//      )
+  
+
+//   // Keyup event listener
+//   $('#textarea').on('keyup', function(event) {
+//     console.log('Keyup event happened!');
+//     // console.log(event);
+//   });
+
+//   // Keypress event listener
+//   $('#t#extarea').on('keypress', function(event) {
+//     console.log('Keypress event happened!');
+//     // console.log(event);
+//   });
+
+//   // Keydown event listener
+//   $('#textarea').on('keydown', function(event) {
+//     console.log('Keydown event happened!');
+//     // console.log(event);
+//   });
+// });
