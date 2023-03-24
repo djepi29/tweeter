b076e3148be79b3640efe3aacf2fd72b812741c5
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-03-24 19:05:26 -0400

    fetching tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,64 +3,213 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-
 $().ready(() => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  
-  const createTweetElement = function(tweet) {
-    let $tweet = $(`
-    <article class="tweet">
-      <header>
-      <p><img src=${tweet.user.avatars}>${tweet.user.name}</p>
-        <p>${tweet.user.handle}</p>
-      </header>
-      <p>${tweet.content.text}</p>
-      <footer>
-      <p>${new Date(tweet.created_at).toLocaleString()}</p>
-        <div>
-              <i class="fa-solid fa-flag"></i>
-              <i class="fa-solid fa-retweet"></i>
-              <i class="fa-solid fa-heart"></i>
-            </div>
-      </footer>
-    </article>
-    `);
-    return $tweet;
-    }
-
   const renderTweets = function(tweets) {
     tweets.forEach(tweet => {
       const $tweet = createTweetElement(tweet);
-      $('#tweet-container').append($tweet);
+      $('#tweet-container').prepend($tweet);
+    });
+    // Update the time display using the timeago library
+    $('p.time').each(function() {
+      $(this).text(timeago.format($(this).attr('datetime')));
     });
   }
-  
-  renderTweets(data);
+
+  const createTweetElement = function(tweet) {
+    let $tweet = $(`
+      <article class="tweet">
+        <header>
+          <p><img src=${tweet.user.avatars}>${tweet.user.name}</p>
+          <p>${tweet.user.handle}</p>
+        </header>
+        <p>${tweet.content.text}</p>
+        <footer>
+          <p class="time" datetime="${new Date(tweet.created_at)}">${new Date(tweet.created_at)}</p>
+          <div>
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `);
+    return $tweet;
+  }
+
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json'
+    })
+    .then(function(response) {
+      renderTweets(response);
+    })
+    .catch(function(error) {
+      console.log('Error loading tweets:', error);
+    });
+  }
+
+  // Add event listener for form submit
+  $('form').submit(function(event) {
+    // Prevent default form submission
+    event.preventDefault();
+    // Serialize form data
+    const formData = $(this).serialize();
+    // Send AJAX POST request to server
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: formData
+    })
+    .then(function() {
+      console.log('Form data submitted successfully');
+      loadTweets();
+    })
+    .catch(function(error) {
+      console.log('Error submitting form data:', error);
+    });
   });
+
+  // loadTweets();
+});
+/////////////////////////////////
+// $().ready(() => {
+//   const data = [
+//     {
+//       "user": {
+//         "name": "Newton",
+//         "avatars": "https://i.imgur.com/73hZDYK.png",
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1461116232227
+//     },
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": "https://i.imgur.com/nlhLi3I.png",
+//         "handle": "@rd"
+//       },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     }
+//   ]
+
+//   const renderTweets = function(tweets) {
+//     tweets.forEach(tweet => {
+//       const $tweet = createTweetElement(tweet);
+//       $('#tweet-container').append($tweet);
+//     });
+//   }
+
+//   const createTweetElement = function(tweet) {
+//     let $tweet = $(`
+//       <article class="tweet">
+//         <header>
+//           <p><img src=${tweet.user.avatars}>${tweet.user.name}</p>
+//           <p>${tweet.user.handle}</p>
+//         </header>
+//         <p>${tweet.content.text}</p>
+//         <footer>
+//           <p>${new Date(tweet.created_at).toLocaleString()}</p>
+//           <div>
+//             <i class="fa-solid fa-flag"></i>
+//             <i class="fa-solid fa-retweet"></i>
+//             <i class="fa-solid fa-heart"></i>
+//           </div>
+//         </footer>
+//       </article>
+//     `);
+//     return $tweet;
+//   }
+
+//   // Add event listener for form submit
+//   $('form').submit(function(event) {
+//     // Prevent default form submission
+//     event.preventDefault();
+//     // Serialize form data
+//     const formData = $(this).serialize();
+//     // Send AJAX POST request to server
+//     $.ajax({
+//       url: '/tweets',
+//       method: 'POST',
+//       data: formData
+//     })
+//     .then(function() {
+//       console.log('Form data submitted successfully', data);
+//     })
+//     .catch(function(error) {
+//       console.log('Error submitting form data:', error);
+//     });
+//   });
+
+//   renderTweets(data);
+// });
+////////////////////////////////////////////////
+// $().ready(() => {
+//   const data = [
+//     {
+//       "user": {
+//         "name": "Newton",
+//         "avatars": "https://i.imgur.com/73hZDYK.png"
+//         ,
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1461116232227
+//     },
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": "https://i.imgur.com/nlhLi3I.png",
+//         "handle": "@rd" },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     }
+//   ]
+  
+//   const renderTweets = function(tweets) {
+//     tweets.forEach(tweet => {
+//       const $tweet = createTweetElement(tweet);
+//       $('#tweet-container').append($tweet);
+//     });
+//   }
+
+//   const createTweetElement = function(tweet) {
+//     let $tweet = $(`
+//     <article class="tweet">
+//       <header>
+//       <p><img src=${tweet.user.avatars}>${tweet.user.name}</p>
+//         <p>${tweet.user.handle}</p>
+//       </header>
+//       <p>${tweet.content.text}</p>
+//       <footer>
+//       <p>${new Date(tweet.created_at).toLocaleString()}</p>
+//         <div>
+//               <i class="fa-solid fa-flag"></i>
+//               <i class="fa-solid fa-retweet"></i>
+//               <i class="fa-solid fa-heart"></i>
+//             </div>
+//       </footer>
+//     </article>
+//     `);
+//     return $tweet;
+//     }
+
+//     $()
+
+  
+//   renderTweets(data);
+//   });
 
 //////////////////////////////////////
 // $().ready(() => {
