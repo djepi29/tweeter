ad2cc80e66c13ad72626e733c1f25cdaecb981ad
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-03-24 13:15:23 -0400

    implement dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,14 +5,105 @@
  */
 
 
+$().ready(() => {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  
+  const createTweetElement = function(tweet) {
+    let $tweet = $(`
+    <article class="tweet">
+      <header>
+      <p><img src=${tweet.user.avatars}>${tweet.user.name}</p>
+        <p>${tweet.user.handle}</p>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+      <p>${new Date(tweet.created_at).toLocaleString()}</p>
+        <div>
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+      </footer>
+    </article>
+    `);
+    return $tweet;
+    }
+
+  const renderTweets = function(tweets) {
+    tweets.forEach(tweet => {
+      const $tweet = createTweetElement(tweet);
+      $('#tweet-container').append($tweet);
+    });
+  }
+  
+  renderTweets(data);
+  });
+
+//////////////////////////////////////
+// $().ready(() => {
+//   const tweetData = {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   };
+  
+//   const $tweet = $(`
+//     <article class="tweet">
+//       <header>
+//       <p><img src="${tweetData.user.avatars}">${tweetData.user.name}</p>
+//         <p>${tweetData.user.handle}</p>
+//       </header>
+//       <p>${tweetData.content.text}</p>
+//       <footer>
+//       <p>${new Date(tweetData.created_at).toLocaleString()}</p>
+//         <div>
+//               <i class="fa-solid fa-flag"></i>
+//               <i class="fa-solid fa-retweet"></i>
+//               <i class="fa-solid fa-heart"></i>
+//             </div>
+//       </footer>
+//     </article>
+//   `);
+
+//   console.log($tweet); 
+//   $('#tweet-container').append($tweet);
+// });
 
 
 
 
 
 
-
-///////////////////////
+////////////////////////////////////////////////
 // texteare practice
 // $().ready(() => {
 
