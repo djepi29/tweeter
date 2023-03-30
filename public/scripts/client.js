a24d79a4349bc6b68c7e724440273e3a578dfd96
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-03-30 18:59:19 -0400

    desktop/tablet breakpoint , client js update, code review update

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -12,13 +12,10 @@ $().ready(() => {
   };
 
   const renderTweets = function(tweets) {
+    $('#tweet-container').empty()
     tweets.forEach(tweet => {
       const $tweet = createTweetElement(tweet);
       $('#tweet-container').prepend($tweet);
-    });
-    // Update the time display using the timeago library
-    $('p.time').each(function() {
-      $(this).text(timeago.format($(this).attr('datetime')));
     });
   }
 
@@ -32,7 +29,7 @@ $().ready(() => {
         </header>
         <p>${escape(tweet.content.text)}</p>
         <footer>
-          <p class="time" datetime="${new Date(tweet.created_at)}">${new Date(tweet.created_at)}</p>
+          <p class="time">${timeago.format(tweet.created_at)}</p>
           <div>
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -58,437 +55,41 @@ $().ready(() => {
     });
   }
 
-  $('form').submit(function(event) {
-    // Prevent default form submission
+
+  $(".error").hide();
+
+  // handle form submission
+  $("form").on("submit", function(event) {
     event.preventDefault();
-    // Serialize form data
-    const formData = $(this).serialize();
+    var tweet = $("textarea").val();
 
-
-    // Validate tweet content
-    const tweetContent = $(this).find('textarea[name="text"]').val();
-    // console.log(tweetContent)
-    if (!tweetContent || tweetContent.length > 140) {
-      let errorMessage;
-      if (!tweetContent) {
-        errorMessage = "Tweet content is not present";
-      } else {
-        errorMessage = "Tweet content is too long";
-      }
-      // Show error message
-      // $(this).find('.error').text(errorMessage);
-      alert(errorMessage);
-      return;
+    // validate tweet length
+    if (tweet.length > 140) {
+      // show error message with slide animation
+      $(".error").text("Your tweet is too long!").slideDown();
+    } else if (tweet.length === 0) {
+      // show error message with slide animation
+      $(".error").text("You haven't entered anything!").slideDown();
+    } else {
+      // hide error message if it's visible
+      $(".error").slideUp();
+      // submit form data
+      $.ajax({
+        method: "POST",
+        url: "/tweets",
+        data: $(this).serialize()
+      })
+      .then(function() {
+        // reload tweets and clear input field
+        loadTweets();
+        $("textarea").val("");
+        $(".counter").val('140');
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
     }
-    // Clear error message
-    $(this).find('.error').text('');
-    // Send AJAX POST request to server
-
-
-    $.ajax({
-      url: '/tweets',
-      method: 'POST',
-      data: formData
-    })
-    .then(function() {
-      console.log('Form data submitted successfully');
-      loadTweets();
-    })
-    .catch(function(error) {
-      console.log('Error submitting form data:', error);
-    });
   });
 
   loadTweets();
 });
-/////////////////////////////
-// $().ready(() => {
-//   const renderTweets = function(tweets) {
-//     tweets.forEach(tweet => {
-//       const $tweet = createTweetElement(tweet);
-//       $('#tweet-container').prepend($tweet);
-//     });
-//     // Update the time display using the timeago library
-//     $('p.time').each(function() {
-//       $(this).text(timeago.format($(this).attr('datetime')));
-//     });
-//   }
-
-
-//   const createTweetElement = function(tweet) {
-//     let $tweet = $(`
-//       <article class="tweet">
-//         <header>
-//           <p><img src=${tweet.user.avatars}>${tweet.user.name}</p>
-//           <p>${tweet.user.handle}</p>
-//         </header>
-//         <p>${tweet.content.text}</p>
-//         <footer>
-//           <p class="time" datetime="${new Date(tweet.created_at)}">${new Date(tweet.created_at)}</p>
-//           <div>
-//             <i class="fa-solid fa-flag"></i>
-//             <i class="fa-solid fa-retweet"></i>
-//             <i class="fa-solid fa-heart"></i>
-//           </div>
-//         </footer>
-//       </article>
-//     `);
-//     return $tweet;
-//   }
-
-//   const loadTweets = function() {
-//     $.ajax({
-//       url: '/tweets',
-//       method: 'GET',
-//       dataType: 'json'
-//     })
-//     .then(function(response) {
-//       renderTweets(response);
-//     })
-//     .catch(function(error) {
-//       console.log('Error loading tweets:', error);
-//     });
-//   }
-
-// $('form').submit(function(event) {
-//   // Prevent default form submission
-//   event.preventDefault();
-//   // Serialize form data
-//   const formData = $(this).serialize();
-
-
-//   // Validate tweet content
-//   const tweetContent = $(this).find('textarea[name="text"]').val();
-//   // console.log(tweetContent)
-//   if (!tweetContent || tweetContent.length > 140) {
-//     let errorMessage;
-//     if (!tweetContent) {
-//       errorMessage = "Tweet content is not present";
-//     } else {
-//       errorMessage = "Tweet content is too long";
-//     }
-//     // Show error message
-//     // $(this).find('.error').text(errorMessage);
-//     alert(errorMessage);
-//     return;
-//   }
-//   // Clear error message
-//   $(this).find('.error').text('');
-//   // Send AJAX POST request to server
-
-
-//   $.ajax({
-//     url: '/tweets',
-//     method: 'POST',
-//     data: formData
-//   })
-//   .then(function() {
-//     console.log('Form data submitted successfully');
-//     loadTweets();
-//   })
-//   .catch(function(error) {
-//     console.log('Error submitting form data:', error);
-//   });
-// });
-
-// loadTweets();
-
-// });
-
-//////////////////////////////////////////////
-// $().ready(() => {
-//   const renderTweets = function(tweets) {
-//     tweets.forEach(tweet => {
-//       const $tweet = createTweetElement(tweet);
-//       $('#tweet-container').prepend($tweet);
-//     });
-//     // Update the time display using the timeago library
-//     $('p.time').each(function() {
-//       $(this).text(timeago.format($(this).attr('datetime')));
-//     });
-//   }
-
-//   const createTweetElement = function(tweet) {
-//     let $tweet = $(`
-//       <article class="tweet">
-//         <header>
-//           <p><img src=${tweet.user.avatars}>${tweet.user.name}</p>
-//           <p>${tweet.user.handle}</p>
-//         </header>
-//         <p>${tweet.content.text}</p>
-//         <footer>
-//           <p class="time" datetime="${new Date(tweet.created_at)}">${new Date(tweet.created_at)}</p>
-//           <div>
-//             <i class="fa-solid fa-flag"></i>
-//             <i class="fa-solid fa-retweet"></i>
-//             <i class="fa-solid fa-heart"></i>
-//           </div>
-//         </footer>
-//       </article>
-//     `);
-//     return $tweet;
-//   }
-
-//   const loadTweets = function() {
-//     $.ajax({
-//       url: '/tweets',
-//       method: 'GET',
-//       dataType: 'json'
-//     })
-//     .then(function(response) {
-//       renderTweets(response);
-//     })
-//     .catch(function(error) {
-//       console.log('Error loading tweets:', error);
-//     });
-//   }
-
-//   // Add event listener for form submit
-//   $('form').submit(function(event) {
-//     // Prevent default form submission
-//     event.preventDefault();
-//     // Serialize form data
-//     const formData = $(this).serialize();
-//     // Send AJAX POST request to server
-//     $.ajax({
-//       url: '/tweets',
-//       method: 'POST',
-//       data: formData
-//     })
-//     .then(function() {
-//       console.log('Form data submitted successfully');
-//       loadTweets();
-//     })
-//     .catch(function(error) {
-//       console.log('Error submitting form data:', error);
-//     });
-//   });
-
-// });
-/////////////////////////////////
-// $().ready(() => {
-//   const data = [
-//     {
-//       "user": {
-//         "name": "Newton",
-//         "avatars": "https://i.imgur.com/73hZDYK.png",
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1461116232227
-//     },
-//     {
-//       "user": {
-//         "name": "Descartes",
-//         "avatars": "https://i.imgur.com/nlhLi3I.png",
-//         "handle": "@rd"
-//       },
-//       "content": {
-//         "text": "Je pense , donc je suis"
-//       },
-//       "created_at": 1461113959088
-//     }
-//   ]
-
-//   const renderTweets = function(tweets) {
-//     tweets.forEach(tweet => {
-//       const $tweet = createTweetElement(tweet);
-//       $('#tweet-container').append($tweet);
-//     });
-//   }
-
-//   const createTweetElement = function(tweet) {
-//     let $tweet = $(`
-//       <article class="tweet">
-//         <header>
-//           <p><img src=${tweet.user.avatars}>${tweet.user.name}</p>
-//           <p>${tweet.user.handle}</p>
-//         </header>
-//         <p>${tweet.content.text}</p>
-//         <footer>
-//           <p>${new Date(tweet.created_at).toLocaleString()}</p>
-//           <div>
-//             <i class="fa-solid fa-flag"></i>
-//             <i class="fa-solid fa-retweet"></i>
-//             <i class="fa-solid fa-heart"></i>
-//           </div>
-//         </footer>
-//       </article>
-//     `);
-//     return $tweet;
-//   }
-
-//   // Add event listener for form submit
-//   $('form').submit(function(event) {
-//     // Prevent default form submission
-//     event.preventDefault();
-//     // Serialize form data
-//     const formData = $(this).serialize();
-//     // Send AJAX POST request to server
-//     $.ajax({
-//       url: '/tweets',
-//       method: 'POST',
-//       data: formData
-//     })
-//     .then(function() {
-//       console.log('Form data submitted successfully', data);
-//     })
-//     .catch(function(error) {
-//       console.log('Error submitting form data:', error);
-//     });
-//   });
-
-//   renderTweets(data);
-// });
-////////////////////////////////////////////////
-// $().ready(() => {
-//   const data = [
-//     {
-//       "user": {
-//         "name": "Newton",
-//         "avatars": "https://i.imgur.com/73hZDYK.png"
-//         ,
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1461116232227
-//     },
-//     {
-//       "user": {
-//         "name": "Descartes",
-//         "avatars": "https://i.imgur.com/nlhLi3I.png",
-//         "handle": "@rd" },
-//       "content": {
-//         "text": "Je pense , donc je suis"
-//       },
-//       "created_at": 1461113959088
-//     }
-//   ]
-  
-//   const renderTweets = function(tweets) {
-//     tweets.forEach(tweet => {
-//       const $tweet = createTweetElement(tweet);
-//       $('#tweet-container').append($tweet);
-//     });
-//   }
-
-//   const createTweetElement = function(tweet) {
-//     let $tweet = $(`
-//     <article class="tweet">
-//       <header>
-//       <p><img src=${tweet.user.avatars}>${tweet.user.name}</p>
-//         <p>${tweet.user.handle}</p>
-//       </header>
-//       <p>${tweet.content.text}</p>
-//       <footer>
-//       <p>${new Date(tweet.created_at).toLocaleString()}</p>
-//         <div>
-//               <i class="fa-solid fa-flag"></i>
-//               <i class="fa-solid fa-retweet"></i>
-//               <i class="fa-solid fa-heart"></i>
-//             </div>
-//       </footer>
-//     </article>
-//     `);
-//     return $tweet;
-//     }
-
-//     $()
-
-  
-//   renderTweets(data);
-//   });
-
-//////////////////////////////////////
-// $().ready(() => {
-//   const tweetData = {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   };
-  
-//   const $tweet = $(`
-//     <article class="tweet">
-//       <header>
-//       <p><img src="${tweetData.user.avatars}">${tweetData.user.name}</p>
-//         <p>${tweetData.user.handle}</p>
-//       </header>
-//       <p>${tweetData.content.text}</p>
-//       <footer>
-//       <p>${new Date(tweetData.created_at).toLocaleString()}</p>
-//         <div>
-//               <i class="fa-solid fa-flag"></i>
-//               <i class="fa-solid fa-retweet"></i>
-//               <i class="fa-solid fa-heart"></i>
-//             </div>
-//       </footer>
-//     </article>
-//   `);
-
-//   console.log($tweet); 
-//   $('#tweet-container').append($tweet);
-// });
-
-
-
-
-
-
-////////////////////////////////////////////////
-// texteare practice
-// $().ready(() => {
-
-
-//   // Input event listener
-//   $('#textarea').on('input', function(event) {
-//     console.log('Input event happened!');
-//     // console.log(event);
-//   });
- 
-//   // Input event listener
-//   $('#textarea').on('blur', function(event) {
-//     console.log('focus event happened!');
-//     // console.log(event);
-//   });
-
-//   // Input event listener
-//   $('#textarea').on('focus', function(event) {
-//     console.log('focus event happened!');
-//     // console.log(event);
-//   });
- 
-//   // Input event listener
-//   $('#textarea').hover(
-//     (event) => {console.log('handlerIn')},
-//     (event) => {console.log('handlerOut')}
-//      )
-  
-
-//   // Keyup event listener
-//   $('#textarea').on('keyup', function(event) {
-//     console.log('Keyup event happened!');
-//     // console.log(event);
-//   });
-
-//   // Keypress event listener
-//   $('#t#extarea').on('keypress', function(event) {
-//     console.log('Keypress event happened!');
-//     // console.log(event);
-//   });
-
-//   // Keydown event listener
-//   $('#textarea').on('keydown', function(event) {
-//     console.log('Keydown event happened!');
-//     // console.log(event);
-//   });
-// });
